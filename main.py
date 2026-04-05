f2bb6c4acf7110266dd3282b833ecf94be1455ef
Zalewa <zalewski.ba@gmail.com>
2026-04-05 15:15:21 +0200

    Subscribe to test network & Get info about new blocks

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-if __name__ == '__main__':
-    pass
+import asyncio
+from infura_monitor import monitor_new_blocks
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(monitor_new_blocks())
+    except KeyboardInterrupt:
+        pass
