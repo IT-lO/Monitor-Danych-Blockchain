6ab11983a2d8cb42387d18ff741b1d9b60ad3484
Zalewa <zalewski.ba@gmail.com>
2026-04-14 12:37:24 +0200

    Provide info about last 100 blocks on start

--- infura_monitor.py
+++ infura_monitor.py
@@ -1,3 +1,4 @@
+import asyncio
 from pathlib import Path
 
 from web3 import AsyncWeb3
@@ -21,7 +22,7 @@ async def monitor_new_blocks():
             subscription_id = await web3.eth.subscribe("newHeads")
             print(f"Subscription ID: {subscription_id}")
 
-            await web3.eth.get_block("latest")
+            await get_100_last_blocks(web3)
 
             async for response in web3.socket.process_subscriptions():
                 number = response["result"].get("number")
@@ -33,3 +34,25 @@ async def monitor_new_blocks():
                 stats_path = save_aggregated_stats(output_dir, "aggregated_stats.json")
                 print(f"Saved block to file: {output_path}")
                 print(f"Updated aggregated stats: {stats_path}")
+
+
+async def get_100_last_blocks(web3: AsyncWeb3):
+    output_dir = Path("filtered_blocks")
+    output_dir.mkdir(exist_ok=True)
+
+    latest_block_number = await web3.eth.block_number
+    block_numbers = range(latest_block_number - 99, latest_block_number + 1)
+
+    tasks = [web3.eth.get_block(block_number, full_transactions=True) for block_number in block_numbers]
+    blocks = await asyncio.gather(*tasks)
+
+    for block in blocks:
+        block_number = block.get("number")
+        block_json = filtered_block_to_json(block)
+
+        output_path = output_dir / f"block_{block_number}.json"
+        output_path.write_text(block_json, encoding="utf-8")
+        print(f"Saved block to file: {output_path}")
+
+    stats_path = save_aggregated_stats(output_dir, "aggregated_stats.json")
+    print(f"Updated aggregated stats: {stats_path}")
